b961a87f074ea0c169e8e2606be1f12817f182c7
Kevin Liebholz <liebholz@crowddesk.de>
2021-04-04 22:23:02 +0200

    [CONFIG-FIX] postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,12 +58,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "daily_bits_production"
 
   config.action_mailer.perform_caching = false
-
   config.action_mailer.delivery_method = :postmark
-
-  config.action_mailer.postmark_settings = {
-    api_token: Rails.application.credentials.postmark_api_token
-  }
+  config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_KEY'] }
+  config.action_mailer.default_url_options = { host: "daily-bits.kevin-liebholz.me" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
