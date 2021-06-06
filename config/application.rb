05a812ce1c45de6c619876850d844ee4ff05a0bd
Kevin Liebholz <liebholz@crowddesk.de>
2021-06-06 19:53:48 +0200

    [CONFIG] locals

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module DailyBits
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
